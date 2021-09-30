3619dd7afe5e28382b210cb16a53774812a3e53d
MANZA <36519974+zaimanza@users.noreply.github.com>
2021-09-30 12:51:18 +0800

    ap

--- app.js
+++ app.js
@@ -59,6 +59,24 @@ app.use(cors({
     credentials: true
 }));
 app.use(rootAuth);
+app.use(function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader(
+        "Access-Control-Allow-Origin",
+        "https://studio.apollographql.com"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Methods, Access-Control-Allow-Origin, Access-Control-Allow-Credentials, Access-Control-Allow-Headers"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "POST, GET, PUT, PATCH, DELETE, OPTIONS, HEAD"
+    );
+
+    // Pass to next layer of middleware
+    next();
+});
 
 var server = null;
 
